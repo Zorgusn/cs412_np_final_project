97aa2f0583ec4de560b8bc1bced3c4d0791b85f0
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-04-23 10:49:13 -0400

    update test cases script

--- code_solution/run_test_cases.sh
+++ code_solution/run_test_cases.sh
@@ -1,9 +1,9 @@
 ## Exact Solution Test Cases
 
 # run exact solution with the simple test case (this is the provided one)
-python exact_solution.py < test_cases/input/simple.txt > test_cases/output/simple_actual.txt
+python exact_solution.py < test_cases/input/simple.txt > test_cases/output/simple_actual_exact.txt
 # diff the simple expected and actual
-diff test_cases/output/simple_actual.txt test_cases/output/simple_expected.txt > test_cases/diffs/simple_diff.txt
+diff test_cases/output/simple_actual_exact.txt test_cases/output/simple_expected.txt > test_cases/diffs/simple_exact_diff.txt
 
 
 
